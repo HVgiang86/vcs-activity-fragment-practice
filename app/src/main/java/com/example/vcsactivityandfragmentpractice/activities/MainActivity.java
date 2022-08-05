368809e2380638f4bc154b7f69c60cda41e9a298
hvgiang86 <hvgiang86@gmail.com>
2022-08-05 16:59:15 +0700

    check

--- app/src/main/java/com/example/vcsactivityandfragmentpractice/activities/MainActivity.java
+++ app/src/main/java/com/example/vcsactivityandfragmentpractice/activities/MainActivity.java
@@ -1,25 +1,33 @@
 package com.example.vcsactivityandfragmentpractice.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.vcsactivityandfragmentpractice.R;
+import com.example.vcsactivityandfragmentpractice.fragments.DeviceInfoFragment;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private int mTextViewColor;
     private TextView mTextView;
     private final String COLOR_KEY = "color_key";
     private final int TIME_INTERVAL = 2000;     // 2000 milliseconds: Time passed between two back pressed
     private long mBackPressedTime;
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,44 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //now tool bar likes an action bar
 
+        //setup navigation drawer
+        mDrawer = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawer, toolbar,
+                                                                            R.string.navigation_drawer_open,
+                                                                            R.string.navigation_drawer_close);
+
+        if (mDrawer != null) {
+            mDrawer.addDrawerListener(mToggle);
+        }
+        mToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        if (navigationView != null) {
+            //Handle when navigation drawer menu item selected
+            MenuItem showDeviceInfoAction = navigationView.getMenu().findItem(R.id.show_device_information);
+            MenuItem searchForApplicationAction = navigationView.getMenu().findItem(R.id.search_for_application);
+
+            showDeviceInfoAction.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    mDrawer.closeDrawer(GravityCompat.START);
+                    Toast.makeText(getApplicationContext(), "Device Info Action Selected!",Toast.LENGTH_SHORT).show();
+                    showDeviceInformation();
+                    return true;
+                }
+            });
+
+            searchForApplicationAction.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    mDrawer.closeDrawer(GravityCompat.START);
+                    Toast.makeText(getApplicationContext(), "Search For Application Action Selected!",Toast.LENGTH_SHORT).show();
+                    searchForAnApplication();
+                    return true;
+                }
+            });
+        }
 
     }
 
@@ -63,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         mTextView.setTextColor(mTextViewColor);
     }
 
-    //handle back button press event
+    /*//handle back button press event
     @Override
     public void onBackPressed() {
 
@@ -76,9 +122,29 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Nhấn nút back lần nữa để thoát ứng dụng!", Toast.LENGTH_SHORT).show();
         }
         mBackPressedTime = System.currentTimeMillis();
+        super.onBackPressed();
+    }*/
+
+
+
+    //Show device information with a fragment
+    public void showDeviceInformation() {
+        DeviceInfoFragment deviceInfoFragment = DeviceInfoFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, deviceInfoFragment)
+                .addToBackStack(null)
+                .commit();
+
     }
 
+    //Show Application Searching UI with a fragment
+    public void searchForAnApplication() {
 
+    }
 
-
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mToggle.onConfigurationChanged(newConfig);
+    }
 }
--- app/src/main/java/com/example/vcsactivityandfragmentpractice/fragments/DeviceInfoFragment.java
+++ app/src/main/java/com/example/vcsactivityandfragmentpractice/fragments/DeviceInfoFragment.java
@@ -1,5 +1,6 @@
 package com.example.vcsactivityandfragmentpractice.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.vcsactivityandfragmentpractice.R;
 
@@ -17,14 +19,6 @@ import com.example.vcsactivityandfragmentpractice.R;
  */
 public class DeviceInfoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public DeviceInfoFragment() {
         // Required empty public constructor
@@ -32,35 +26,31 @@ public class DeviceInfoFragment extends Fragment {
 
     /**
      * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DeviceInfoFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static DeviceInfoFragment newInstance(String param1, String param2) {
+    public static DeviceInfoFragment newInstance() {
         DeviceInfoFragment fragment = new DeviceInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        /*if (getArguments() != null) {
+        }*/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_device_info, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_device_info, container, false);
+
+        TextView deviceModelTextView = rootView.findViewById(R.id.device_name_tv);
+        TextView osVersionTextView = rootView.findViewById(R.id.os_version_tv);
+
+        osVersionTextView.setText(System.getProperty("os.version"));
+        deviceModelTextView.setText(Build.MODEL);
+
+        return rootView;
     }
 }
